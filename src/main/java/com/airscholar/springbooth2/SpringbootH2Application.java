0685e5ae9ca9de5f91868747dbc8b31c5badec8c
Yusuf Ganiyu <airscholar@gmail.com>
2021-05-01 22:37:24 +0100

    Updated main entry point to save a sample data in h2 in memory DB

--- src/main/java/com/airscholar/springbooth2/SpringbootH2Application.java
+++ src/main/java/com/airscholar/springbooth2/SpringbootH2Application.java
@@ -1,13 +1,24 @@
 package com.airscholar.springbooth2;
 
+import com.airscholar.springbooth2.model.Person;
+import com.airscholar.springbooth2.repository.PersonRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootH2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootH2Application.class, args);
+
+        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(SpringbootH2Application.class, args);
+
+        PersonRepository personRepository = configurableApplicationContext.getBean(PersonRepository.class);
+
+        Person person = new Person("John", "Doe");
+        personRepository.save(person);
+
+
     }
 
 }
